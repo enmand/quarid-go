66c3452f199bc649df2dd91be36d4f065898185c
Daniel Enman <enmand@gmail.com>
2015-01-22 14:57:16 -0400

    Add plugin system

---
+++ plugin.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/thoj/go-ircevent"
+)
+
+var availablePlugins map[string]Plugin
+var activePlugins map[string]Plugin
+
+func init() {
+	availablePlugins = map[string]Plugin{}
+	activePlugins = map[string]Plugin{}
+}
+
+type Command struct {
+	Handler func(con *irc.Connection, config *Config, m chan *Message)
+}
+
+type Plugin struct {
+	Commands map[*regexp.Regexp]Command
+}
+
+func RegisterPlugin(name string, plugin Plugin) {
+	fmt.Printf("Loading plugin %s\n", name)
+
+	availablePlugins[name] = plugin
+}
+
+func InitializePlugin(config *Config) {
+	for _, name := range config.Plugins {
+		if plugin, ok := availablePlugins[name]; ok {
+			fmt.Printf("Activating plugin: %v\n", name)
+			activePlugins[name] = plugin
+		}
+	}
+}
+
+func findCommand(msgCh chan *Message, cmdCh chan Command) {
+	msg := <-msgCh
+
+	for _, plugin := range activePlugins {
+		go func(plugin Plugin) {
+			for trigger, cmd := range plugin.Commands {
+				if trigger.Match([]byte(msg.Message())) {
+					cmdCh <- cmd
+				}
+			}
+		}(plugin)
+	}
+}
