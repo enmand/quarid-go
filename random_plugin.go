b85b0bd4a5d454ba1dd3a8693f740a365cda1a93
Daniel Enman <enmand@gmail.com>
2015-01-22 14:57:50 -0400

    Add "random" plugin, for random number generation

---
+++ random_plugin.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"time"
+
+	"github.com/thoj/go-ircevent"
+)
+
+func init() {
+	const PLUGIN_NAME = "random"
+
+	trigger, err := regexp.Compile("^@random")
+	if err != nil {
+		fmt.Errorf("Could not load plugin %s", PLUGIN_NAME)
+	}
+
+	commands := map[*regexp.Regexp]Command{
+		trigger: Command{
+			Handler: randomHandler,
+		},
+	}
+
+	plugin := Plugin{
+		Commands: commands,
+	}
+
+	RegisterPlugin(PLUGIN_NAME, plugin)
+}
+
+func randomHandler(con *irc.Connection, config *Config, msgCh chan *Message) {
+	msg := <-msgCh
+	rand.Seed(time.Now().Unix())
+	rand := rand.ExpFloat64()
+	con.Privmsgf(msg.Room, "%f", rand)
+}
