cf072bd182517d03e6a781695b58b1f8e6731f68
Dan Enman <enmand@gmail.com>
2015-03-30 10:24:30 -0300

    Use Bot for connecting to IRC server

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
+
+	"github.com/enmand/quarid-go/services"
 
 	"github.com/alecthomas/kingpin"
 	"github.com/thoj/go-ircevent"
 )
 
-var (
-	debug      = kingpin.Flag("debug", "Debug mode").Bool()
-	configFile = kingpin.Flag("config", "Configuration file").Required().String()
-)
-
 const VERSION = "0.1"
+
+var (
+	configFile = kingpin.Flag(
+		"config",
+		"Configuration file",
+	).Required().String()
+)
 
 type Message struct {
 	*irc.Event
@@ -24,48 +27,18 @@ type Message struct {
 func main() {
 	kingpin.Version(VERSION)
 	kingpin.Parse()
-	config, err := loadConfig(*configFile)
+
+	var err error
+	config, err := services.NewConfig(*configFile)
 	if err != nil {
-		fmt.Errorf("Could not load configuration: %s\n", err)
+		fmt.Printf("Could not load configuration: %s\n", err)
 		return
 	}
-	fmt.Printf("Loading plugins...")
-	InitializePlugin(config)
 
-	con := irc.IRC(config.Nick, config.User)
-	con.Debug = *debug
+	bot := NewBot(config)
 
-	con.UseTLS = config.TLS.Enable
-	con.TLSConfig = &tls.Config{InsecureSkipVerify: !config.TLS.Verify}
-	if err := con.Connect(config.Server); err != nil {
-		fmt.Printf("%#v", err)
-	}
-	defer con.Disconnect()
+	bot.Connect()
+	defer bot.Disconnect()
 
-	con.AddCallback("CTCP_VERSION", func(e *irc.Event) {
-		con.Noticef(e.Nick, "\x01VERSION Quarid %s", VERSION)
-	})
-
-	con.AddCallback("001", func(e *irc.Event) {
-		for _, room := range config.Channels {
-			con.Join(room)
-		}
-	})
-
-	con.AddCallback("PRIVMSG", func(event *irc.Event) {
-		go func(e *irc.Event) {
-			msg := &Message{e, e.Arguments[0]}
-
-			msgCh := make(chan *Message)
-			cmdCh := make(chan Command)
-			go findCommand(msgCh, cmdCh)
-
-			msgCh <- msg
-			_cmd := <-cmdCh
-			go _cmd.Handler(con, config, msgCh)
-
-		}(event)
-	})
-
-	con.Loop()
+	bot.Connection.Loop()
 }
