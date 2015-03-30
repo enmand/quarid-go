66da460bc73ee72c47b14abcc7d8589c69adc4de
Dan Enman <enmand@gmail.com>
2015-03-30 10:00:14 -0300

    Move Database to services

---
+++ services/database.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/boltdb/bolt"
+)
+
+const DB_DIR = "dbs/%s"
+const DB_FILEPATH = "%s.db"
+
+type Database *bolt.DB
+
+func GetDatabase(dir, path string) (Database, error) {
+	file := fmt.Sprintf("%s/%s/%s.db", DB_DIR, dir, DB_FILEPATH)
+	_, err := os.Stat(file)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open '%s': %s", file, err)
+	}
+
+	full_dir := fmt.Sprintf(DB_DIR, dir)
+	if err := os.Mkdir(dir, os.ModeDir); err != nil {
+		return nil, fmt.Errorf(
+			"Could not create DB directory %s: %s",
+			full_dir,
+			err,
+		)
+	}
+	return bolt.Open(file, 0600, nil)
+}
