9c1f834c3365f2ae3790f3255159079c5601875d
Dan Enman <enmand@gmail.com>
2015-05-24 19:40:03 -0300

    Remove dead code from old IRC library

--- services/bot/bot.go
+++ services/bot/bot.go
@@ -8,8 +8,6 @@ import (
 	"github.com/enmand/quarid-go/vm"
 )
 
-type Event interface{}
-
 type Bot interface {
 	// Load all of the plugins in each `dir`
 	LoadPlugins(dir []string) ([]plugin.Plugin, []error)
@@ -20,19 +18,11 @@ type Bot interface {
 	// Disconnect from the server
 	Disconnect()
 
-	// Add a callback based on an IRC event
-	AddCallback(event string, f func(e *Event)) string
-
-	// Clear all callbacks
-	ClearCallback(event string) bool
-
 	// Return a map of initialzed Plugins for this bot
 	Plugins() []plugin.Plugin
 
 	// A list of VMs that the bot has available
 	VMs() map[string]vm.VM
-
-	Read(ch chan Event)
 }
 
 func New(cfg *config.Config) *quarid {
