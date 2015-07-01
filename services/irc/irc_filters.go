29c1f0d17cedbdefe8ba00f33cda4f8855ddad5f
Dan Enman <enmand@gmail.com>
2015-07-01 19:08:51 -0300

    govet/golint cleanup

--- services/irc/irc_filters.go
+++ services/irc/irc_filters.go
@@ -1,18 +1,25 @@
+package irc
+
 // Filters
 //
 // Filters define ways to match Events from the server to some matching pattern,
 // so that specific events can trigger specific actions (or not trigger actions)
-package irc
 
 // Filter is an interface that matches Events
 type Filter interface {
 	Match(ev *Event) bool
 }
 
+// CommandFilter filters events based on the IRC command of the event
 type CommandFilter struct {
 	Command string
 }
 
+// Match this filter, against incoming events.
 func (cf CommandFilter) Match(ev *Event) bool {
+	if cf.Command == "*" || cf.Command == "" {
+		// Match all events
+		return true
+	}
 	return cf.Command == ev.Command
 }
