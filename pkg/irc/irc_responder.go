c80e913134570723ee85c23c674e204be6ccd3e1
lirancohen <lirancohen@me.com>
2015-11-15 10:17:00 -0500

    What is this garbage

--- pkg/irc/irc_responder.go
+++ pkg/irc/irc_responder.go
@@ -20,13 +20,12 @@ func (i *Client) Write(ev *adapter.Event) error {
 
 	payload = append(payload, []byte(ev.Command))
 	for i, p := range ev.Parameters {
-		if i == len(ev.Parameters)-1 && len(ev.Parameters) > 1 {
+		if i == len(ev.Parameters)-1 && len(ev.Parameters) > 0 {
 			p = fmt.Sprintf(":%s\r\n", p)
 		}
 		payload = append(payload, []byte(p))
 	}
 
-	payload = append(payload, []byte("\r\n"))
 	full := bytes.Join(payload, []byte(" "))
 
 	_, err := i.conn.Write(full)
