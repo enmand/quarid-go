f2d6599f15c4aa197d82f6d6ebc4439ce34d7b2f
lirancohen <lirancohen@me.com>
2015-11-15 13:15:53 -0500

    fixed it

--- pkg/irc/irc_responder.go
+++ pkg/irc/irc_responder.go
@@ -20,12 +20,13 @@ func (i *Client) Write(ev *adapter.Event) error {
 
 	payload = append(payload, []byte(ev.Command))
 	for i, p := range ev.Parameters {
-		if i == len(ev.Parameters)-1 && len(ev.Parameters) > 0 {
-			p = fmt.Sprintf(":%s\r\n", p)
+		if i == len(ev.Parameters)-1 && len(ev.Parameters) > 1 {
+			p = fmt.Sprintf(":%s", p)
 		}
 		payload = append(payload, []byte(p))
 	}
 
+	payload = append(payload, []byte("\r\n"))
 	full := bytes.Join(payload, []byte(" "))
 
 	_, err := i.conn.Write(full)
