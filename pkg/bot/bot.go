d5a84fd5d2562e48e5e30e8505301110f6c60a92
Dan Enman <enmand@gmail.com>
2018-02-18 22:38:12 -0400

    Move Start/Stop into main bot definitions

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -19,3 +19,11 @@ func New(a adapter.Adapter) *Bot {
 		adapter: a,
 	}
 }
+
+func (b *Bot) Start() error {
+	return b.adapter.Start()
+}
+
+func (b *Bot) Stop() error {
+	return b.adapter.Stop()
+}
