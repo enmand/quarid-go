2a3e93a5d70f6448031125486f69fcf091804afb
Dan Enman <enmand@gmail.com>
2018-02-28 23:34:30 -0400

    WIP plugin engine for JavaScript

--- pkg/plugin/javascript.go
+++ pkg/plugin/javascript.go
@@ -1,1 +1,54 @@
 package plugin
+
+import (
+	"github.com/enmand/quarid-go/pkg/adapter"
+	"github.com/enmand/quarid-go/pkg/engines"
+	"github.com/enmand/quarid-go/pkg/engines/js"
+	"github.com/pkg/errors"
+	"github.com/robertkrimen/otto"
+)
+
+type javascript struct {
+	path   string
+	source string
+	jsvm   engines.Engine
+}
+
+func newJavaScript(path string, source string) (plugin, error) {
+	vm := js.New()
+	if err := vm.Load(); err != nil {
+		return nil, errors.Wrap(err, "unable to load javascript engine")
+	}
+
+	return &javascript{
+		path:   path,
+		source: source,
+		jsvm:   vm,
+	}, nil
+}
+
+func (js *javascript) Filters() ([]adapter.Filter, error) {
+	s, err := js.jsvm.Compile(js.path, js.source)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to compile script")
+	}
+	os := s.(otto.Value)
+	obj := os.Object()
+
+	fs := []*otto.Object{}
+
+	for _, k := range obj.Keys() {
+		v, err := obj.Get(k)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to parse filter keys")
+		}
+		f, err := v.Object().Get("filters")
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to find filters")
+		}
+
+		fs = append(fs, f.Object())
+	}
+
+	return nil, nil
+}
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -1,39 +1,83 @@
 package plugin
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/enmand/quarid-go/pkg/adapter"
 	"github.com/enmand/quarid-go/pkg/engines"
 	"github.com/pkg/errors"
+	"strings"
 )
-
-// Type defines the runtime to use for the given Plugin
-type Type string
-
-var ()
 
 // Plugin is a runnable set of instructions, that can react to an event
 type Plugin struct {
-	pluginType Type
+	pluginType engines.Type
+	plugin     plugin
 	path       string
 	filters    []adapter.Filter
 }
 
+type plugin interface {
+	Filters() ([]adapter.Filter, error)
+}
+
 // LoadPlugins a set of plugins from a path
-func LoadPlugins(path []string) ([]Plugin, error) {
+func LoadPlugins(path []string) ([]*Plugin, error) {
+	ps := []*Plugin{}
+
 	for _, dir := range path {
 		finfos, err := ioutil.ReadDir(dir)
 		if err != nil {
 			return nil, errors.Wrap(err, "unable to read plugin dir")
 		}
 		for _, plugin := range finfos {
+			path := fmt.Sprintf("%s/%s", dir, plugin.Name())
 			if plugin.IsDir() {
 				// load plugin dir
 			} else {
-				// load plugin file
+				p, err := Load(path)
+				if err != nil {
+					return nil, err
+				}
+				ps = append(ps, p)
 			}
 		}
+	}
+
+	return ps, nil
+}
+
+// Load loads a plugin from a file source
+func Load(path string) (*Plugin, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to open file")
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't read file")
+	}
+
+	if strings.HasSuffix(path, ".js") {
+		jsplugin, err := newJavaScript(path, string(b))
+		if err != nil {
+			return nil, err
+		}
+
+		f, err := jsplugin.Filters()
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to load javascript plugin filters")
+		}
+
+		return &Plugin{
+			plugin:     jsplugin,
+			pluginType: engines.JS,
+			path:       path,
+			filters:    f,
+		}, nil
 	}
 
 	return nil, nil
